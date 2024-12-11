dd1fbd6eeb42b344b5041257bb2adfc503741232
sarthak jain <99114224+sartq333@users.noreply.github.com>
2024-12-11 20:37:14 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import gradio as gr
 from face_parsing import integration_with_meme
 
 def replace_face(person_image, face_x, face_y, face_width, face_height):
-    result_image = integration_with_meme(input_img_path=person_image, face_x=face_x, face_y=face_y, face_width=face_width, face_height=face_height)
+    result_image = integration_with_meme(input_img_path=person_image, face_x=face_x, face_y=face_y, face_width=face_width, face_height=face_height, flip=flip)
     return result_image
 
 interface = gr.Interface(
@@ -13,6 +13,7 @@ interface = gr.Interface(
         gr.Slider(0, 500, 40, step=1, label="Face Y Coordinate"),
         gr.Slider(10, 200, 100, step=1, label="Face Width"),
         gr.Slider(10, 200, 100, step=1, label="Face Height"),
+        gr.Checkbox(label="Flip Person Image Horizontally"),
     ],
     outputs=gr.Image(type="pil", label="Result Image"),
     allow_flagging="never",
